a73b39635411dcdccc97ce9d898791c64981a39b
Albin <albin@systemfabriken.tech>
2023-11-02 22:56:04 +0000

    Started adding Zerv PUB-SUB functionality

--- include/zephyr/zerv/zerv.h
+++ include/zephyr/zerv/zerv.h
@@ -226,6 +226,35 @@
 					&__##zervice##_fifo, 0)
 
 /*=================================================================================================
+ * ZERV PUB-SUB MACROS
+ *===============================================================================================*/
+
+/**
+ * @brief Macro for declaring a zervice publisher topic in a header file.
+ *
+ * @param name The name of the topic.
+ * @param params... The parameters of the topic. Should be declared with the ZERV_IN macro.
+ */
+#define ZERV_PUB_TOPIC_DECL(name, params...)
+
+/**
+ * @def ZERV_PUB_TOPIC_EMIT(name, params...)
+ * @brief Macro for emitting a message on a Zerv publisher topic.
+ *
+ * This macro is used to emit a message on a Zerv publisher topic. The name of the topic
+ * and any parameters to be included in the message are passed as arguments to the macro.
+ *
+ * Example usage:
+ * @code{.c}
+ * ZERV_PUB_TOPIC_EMIT(my_topic, "Hello, world!", 42);
+ * @endcode
+ *
+ * @param name The name of the Zerv publisher topic to emit the message on.
+ * @param params The parameters to include in the message.
+ */
+#define ZERV_PUB_TOPIC_EMIT(name, params...)
+
+/*=================================================================================================
  * ZERVICE CLIENT MACROS
  *===============================================================================================*/
 
